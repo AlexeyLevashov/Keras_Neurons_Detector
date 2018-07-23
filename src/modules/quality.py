f961bea03b8b21289e6be9679f0b8fd92ee039a8
goodgame <alexeylevashov89@gmail.com>
2018-07-23 23:56:16 +0300

    Add seeking best threshold function.

--- src/modules/quality.py
+++ src/modules/quality.py
@@ -1,11 +1,15 @@
 import numpy as np
-from sklearn.metrics import roc_curve, average_precision_score
+from sklearn.metrics import precision_recall_curve, average_precision_score
 
 
 class QualityObject:
     def __init__(self, label, prediction):
         self.label = label
         self.prediction = prediction
+
+
+def f1(precision, recall):
+    return 2*(precision*recall)/(precision + recall)
 
 
 def compute_quality(ground_truth_rects, pred_rects):
@@ -17,28 +21,31 @@ def compute_quality(ground_truth_rects, pred_rects):
         if len(distances) > 0 and np.max(distances) > 0.5:
             nearest_rect_index = int(np.argmax(distances))
             pred_rect = pred_rects[nearest_rect_index]
-            quality_objects.append(QualityObject(1.0, pred_rect.score))
+            quality_objects.append(QualityObject(1, pred_rect.score))
             del pred_rects[nearest_rect_index]
         else:
             missed_rects.append(ground_truth_rect)
     for pred_rect in pred_rects:
-        quality_objects.append(QualityObject(0.0, pred_rect.score))
+        quality_objects.append(QualityObject(0, pred_rect.score))
     for true_rect in missed_rects:
-        quality_objects.append(QualityObject(1.0, 0.0))
+        quality_objects.append(QualityObject(1, 0.0))
 
     return quality_objects
 
 
-def build_roc(quality_objects):
+def find_optimal_threshold(quality_objects):
     true_labels = []
     predictions = []
     for q in quality_objects:
         true_labels.append(q.label)
         predictions.append(q.prediction)
-    fpr, tpr, threshold = roc_curve(true_labels, predictions)
+    precision, recall, thresholds = precision_recall_curve(true_labels, predictions)
+    f1_scores = [(th, f1(precision[i], recall[i])) for i, th in enumerate(thresholds)]
+    f1_scores = sorted(f1_scores, key=lambda x: -x[1])
+    return f1_scores[0]
 
 
-def compute_mAP(quality_objects):
+def compute_average_precision(quality_objects):
     true_labels = []
     predictions = []
     for q in quality_objects:
