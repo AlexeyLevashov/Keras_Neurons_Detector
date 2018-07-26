83c4b312521d3733bf3d973cf2cb8c66c8cfc002
Levashov <alexey.levashov@accenture.com>
2018-07-26 10:59:10 +0300

    Bugfix.

--- src/train.py
+++ src/train.py
@@ -13,7 +13,6 @@ def main():
     fcn_model = loader.get_fcn_model_module().FCNModel()
     trainer = Trainer()
     trainer.train(fcn_model, dataset)
-    K.clear_session()
 
     if not config.one_batch_overfit:
         detector = FCNDetector(fcn_model.model)
